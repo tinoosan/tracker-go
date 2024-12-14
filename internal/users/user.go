0b7db482a53909465e7c4e208763ec4337f50b28
Santino Onyeme <santino.onyeme@hotmail.co.uk>
2024-12-14 16:33:36 +0000

    Add UpdateUserByID implementation for InMemoryStore

--- internal/users/user.go
+++ internal/users/user.go
@@ -16,7 +16,8 @@ type User struct {
 
 type UserRepository interface {
 	AddUser(u *User) error
-  GetUserByID(userId uuid.UUID) (*User, error)
+	GetUserByID(userId uuid.UUID) (*User, error)
+	UpdateUserByID(userID uuid.UUID, username, email string) (*User, error)
 }
 
 type InMemoryStore struct {
@@ -33,7 +34,7 @@ var (
 	ErrUsernameExists    = &Error{message: "Username already exists"}
 	ErrUsernameNull      = &Error{message: "Username cannot be empty"}
 	ErrUsernameInvalid   = &Error{message: "Username contains invalid characters. Only characters a-z, A-Z and spaces are allowed"}
-  ErrUserIdNull        = &Error{message: "User ID cannot be empty"}
+	ErrUserIdNull        = &Error{message: "User ID cannot be empty"}
 	ErrEmailExists       = &Error{message: "Email already exists"}
 	ErrEmailNull         = &Error{message: "Email cannot be empty"}
 	ErrEmailInvalid      = &Error{message: "Email contains invalid characters. Only characters a-z, A-Z and spaces are allowed"}
@@ -57,7 +58,7 @@ func (e *Error) Error() string {
 func NewInMemoryStore() *InMemoryStore {
 	return &InMemoryStore{Users: make(map[uuid.UUID]*User),
 		UserIDToUsername: make(map[string]uuid.UUID),
-  UserIDToEmail: make(map[string]uuid.UUID),}
+		UserIDToEmail:    make(map[string]uuid.UUID)}
 }
 
 func NewUser(username, email, password string) (*User, error) {
@@ -93,32 +94,46 @@ func (s *InMemoryStore) AddUser(user *User) error {
 	if user == nil {
 		return ErrUserNotCreated
 	}
-  _, ok := s.UserIDToEmail[user.Email]
-  if ok {
-    return ErrEmailExists
-  }
+	_, ok := s.UserIDToEmail[user.Email]
+	if ok {
+		return ErrEmailExists
+	}
 
-  _, ok = s.UserIDToUsername[user.Username]
-  if ok {
-    return ErrUsernameExists
-  }
-  s.UserIDToEmail[user.Email] = user.Id
-  s.UserIDToUsername[user.Username] = user.Id
-  s.Users[user.Id] = user
+	_, ok = s.UserIDToUsername[user.Username]
+	if ok {
+		return ErrUsernameExists
+	}
+	s.UserIDToEmail[user.Email] = user.Id
+	s.UserIDToUsername[user.Username] = user.Id
+	s.Users[user.Id] = user
 	return nil
 
 }
 
 func (s *InMemoryStore) GetUserByID(userId uuid.UUID) (*User, error) {
-  if userId.String() == "" {
-    return nil, ErrUserIdNull
-  }
-  user, ok := s.Users[userId]
-  if !ok {
-    return nil, ErrUserNotFound
-  }
+	if userId.String() == "" {
+		return nil, ErrUserIdNull
+	}
+	user, exists := s.userIDExists(userId)
+	if !exists {
+		return nil, ErrUserNotFound
+	}
 
-  return user, nil
+	return user, nil
+}
+
+func (s *InMemoryStore) UpdateUserByID(userId uuid.UUID, username, email string) (*User, error) {
+	user, exists := s.userIDExists(userId)
+	if !exists {
+		return nil, ErrUserNotFound
+	}
+ if username != user.Username {
+    user.Username = username
+  }
+ if email != user.Email {
+    user.Email = email
+  }
+	return user, nil
 }
 
 func isEmailValid(email string) bool {
@@ -137,6 +152,14 @@ func isUsernameValid(username string) bool {
 		return false
 	}
 	return true
+}
+
+func (s *InMemoryStore) userIDExists(userId uuid.UUID) (*User, bool) {
+	user, ok := s.Users[userId]
+	if !ok {
+		return nil, false
+	}
+	return user, true
 }
 
 func isPasswordValid(password string) (bool, error) {
--- internal/users/user_test.go
+++ internal/users/user_test.go
@@ -3,57 +3,71 @@ package users
 import "testing"
 
 var (
-  username = "Testuser1234"
-  email = "testuser@test.com"
-  password = "MyStrongPassword123!"
-  mockMap = NewInMemoryStore()
+	username = "Testuser1234"
+	email    = "testuser@test.com"
+	password = "MyStrongPassword123!"
+	mockMap  = NewInMemoryStore()
 )
 
 func deleteMap() {
-	for k, _ := range mockMap.Users{
+	for k, _ := range mockMap.Users {
 		delete(mockMap.Users, k)
 	}
 
-	for k, _ := range mockMap.UserIDToUsername{
+	for k, _ := range mockMap.UserIDToUsername {
 		delete(mockMap.UserIDToUsername, k)
 	}
 
-  for k, _ := range mockMap.UserIDToEmail {
-    delete(mockMap.UserIDToEmail, k)
-  }
+	for k, _ := range mockMap.UserIDToEmail {
+		delete(mockMap.UserIDToEmail, k)
+	}
 }
 
 func TestNewUser(t *testing.T) {
-  t.Cleanup(deleteMap)
-  user, err := NewUser(username, email, password)
-  if err != nil {
-    t.Error(err)
-  }
+	t.Cleanup(deleteMap)
+	user, err := NewUser(username, email, password)
+	if err != nil {
+		t.Error(err)
+	}
 
-  if user == nil {
-    t.Error(ErrUserNotCreated)
-  }
+	if user == nil {
+		t.Error(ErrUserNotCreated)
+	}
 
 }
 
 func TestAddUser(t *testing.T) {
-  t.Cleanup(deleteMap)
-  user, _ := NewUser(username, email, password)
-  err := mockMap.AddUser(user)
-  if err != nil {
-    t.Error(err)
-  }
+	t.Cleanup(deleteMap)
+	user, _ := NewUser(username, email, password)
+	err := mockMap.AddUser(user)
+	if err != nil {
+		t.Error(err)
+	}
 }
 
 func TestGetUserByID(t *testing.T) {
-  t.Cleanup(deleteMap)
-  user, _:= NewUser(username, email, password)
-  err := mockMap.AddUser(user)
-  if err != nil {
-    t.Error(err)
-  }
-  fetchedUser, _ := mockMap.GetUserByID(user.Id)
-  if fetchedUser.Id != user.Id {
-    t.Errorf("Expected user with ID '%s' but got '%s'", user.Id, fetchedUser.Id)
-  }
+	t.Cleanup(deleteMap)
+	user, _ := NewUser(username, email, password)
+	err := mockMap.AddUser(user)
+	if err != nil {
+		t.Error(err)
+	}
+	fetchedUser, _ := mockMap.GetUserByID(user.Id)
+	if fetchedUser.Id != user.Id {
+		t.Errorf("Expected user with ID '%s' but got '%s'", user.Id, fetchedUser.Id)
+	}
+}
+
+func TestUpdateUserByID(t *testing.T) {
+	t.Cleanup(deleteMap)
+	user, _ := NewUser(username, email, password)
+	err := mockMap.AddUser(user)
+	if err != nil {
+		t.Error(err)
+	}
+	_, err = mockMap.UpdateUserByID(user.Id, "newuser", "newemail@test.com")
+	if err != nil {
+		t.Error(err)
+	}
+	
 }
